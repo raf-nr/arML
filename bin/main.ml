3806096b75879d4ef46c7eb646b176e25074f6d1
raf-nr <nurmuhametovrafik04@gmail.com>
2024-10-22 00:06:55 +0300

    feat: make project structure, add dune files

---
+++ bin/main.ml
@@ -0,0 +1,9 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2024, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
+let () = print_endline "СОСАТЬ АМЕРИКА"
