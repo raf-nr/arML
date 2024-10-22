579c093454321232dec9dcbd364d9ca2d274dba9
ksenmel <ksemelashenko@gmail.com>
2024-10-22 23:41:43 +0300

    feat(ast): add constant and indentifier constructors in ast

--- lib/ast/ast.ml
+++ lib/ast/ast.ml
@@ -1,3 +1,13 @@
 (** Copyright 2024, raf-nr and ksenmel *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+type identifier = string [@@deriving show { with_path = false }]
+
+type constant =
+  | CInt of int (** -1, 0, 1, ... *)
+  | CBool of bool (** true, false *)
+  | CChar of char (** 'a', 'b', 'c', ... *)
+  | CString of string (** "Hello world!" *)
+  | CUnit of unit (** () *)
+[@@deriving show { with_path = false }]
